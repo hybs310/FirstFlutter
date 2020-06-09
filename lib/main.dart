28cc765a164ee8611c0570788888e14d89a4f5f8
hybs310 <hybs310@naver.com>
2020-06-10 08:45:59 +0900

    A markable change on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
           ),
         backgroundColor: Color(0xff05DBF2),
       ),
-    body: MainWindow2(),
+    body: MainWindow(),
     ),
   ));
 }
@@ -27,8 +27,134 @@ final people = [
 
 //-------------------------------
 
-//메인페이지 영역
-class MainWindow extends StatelessWidget{
+
+
+
+//현재 테스트중인 메인페이지 영역
+class MainWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: people.length + 1,
+      itemBuilder: (BuildContext context, int index) {
+        if(index == 0) return HeaderTile();
+        return ListTile(
+          title: Text(people[index]),
+          subtitle: Text('werwer'),
+        );
+      },
+    );
+  }
+}
+
+
+
+//일정 디테일 윈도우
+class DetailWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Scaffold Page',
+          style: TextStyle(fontWeight: FontWeight.w700),
+          ),
+        backgroundColor: Color(0xFF05DBF2),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+             child: FractionallySizedBox(
+                widthFactor: 1,
+                heightFactor: 0.3,
+                child: Container(
+                  color: Colors.blueGrey,
+                  alignment: Alignment.center,
+                  child: Text('Oh my GOD'),
+                ),
+             ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//일정 추가 윈도우
+class AddTodoWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add To-do Page',
+          style: TextStyle(fontWeight: FontWeight.w700),
+          ),
+        backgroundColor: Color(0xFF05DBF2),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+             child: FractionallySizedBox(
+                widthFactor: 1,
+                heightFactor: 0.3,
+                child: Container(
+                  color: Colors.blueGrey,
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.all(10),
+                  child: Text('Oh my GOD'),
+                ),
+             ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//헤더타일 부분
+class HeaderTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network("https://t1.daumcdn.net/thumb/R720x0/?fname=https://t1.daumcdn.net/brunch/service/user/1YN0/image/ak-gRe29XA2HXzvSBowU7Tl7LFE.png"),
+    );
+  }
+}
+
+
+//PersonTile 부분
+class PersonTile extends StatelessWidget {
+  PersonTile(this._person);
+
+  final Person _person;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      trailing: Icon(Icons.arrow_left),
+      title: Text(_person.name),
+      subtitle: Text('${_person.age}살'),
+    );
+  }
+}
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+}
+
+//구 메인윈도우
+class MainWindow_old extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,7 +225,7 @@ class MainWindow extends StatelessWidget{
             onTap: () {
               Navigator.push(
                 context, MaterialPageRoute(
-                  builder: (context) => ScaffoldWindow(),
+                  builder: (context) => DetailWindow(),
                 )
               );
             },
@@ -121,26 +247,8 @@ class MainWindow extends StatelessWidget{
   }
 } 
 
-//현재 테스트중인 메인페이지 영역
-class MainWindow2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: people.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        if(index == 0) return HeaderTile();
-        return ListTile(
-          title: Text(people[index]),
-          subtitle: Text('werwer'),
-        );
-      },
-    );
-  }
-}
-
-////////////////////////////////갖다버릴부분////////////////////////////////
-class DetailWindow extends StatelessWidget {
+//구 디테일윈도우
+class DetailWindow_old extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -157,108 +265,4 @@ class DetailWindow extends StatelessWidget {
       ),
     );
   }
-}
-////////////////////////////////갖다버릴부분////////////////////////////////
-
-
-//일정 디테일 윈도우
-class ScaffoldWindow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Scaffold Page',
-          style: TextStyle(fontWeight: FontWeight.w700),
-          ),
-        backgroundColor: Color(0xFF05DBF2),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-             child: FractionallySizedBox(
-                widthFactor: 1,
-                heightFactor: 0.3,
-                child: Container(
-                  color: Colors.blueGrey,
-                  alignment: Alignment.center,
-                  child: Text('Oh my GOD'),
-                ),
-             ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-//일정 추가 윈도우
-class AddTodoWindow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Add To-do Page',
-          style: TextStyle(fontWeight: FontWeight.w700),
-          ),
-        backgroundColor: Color(0xFF05DBF2),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-             child: FractionallySizedBox(
-                widthFactor: 1,
-                heightFactor: 0.3,
-                child: Container(
-                  color: Colors.blueGrey,
-                  alignment: Alignment.center,
-                  child: Text('Oh my GOD'),
-                ),
-             ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-//헤더타일 부분
-class HeaderTile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Image.network("https://t1.daumcdn.net/thumb/R720x0/?fname=https://t1.daumcdn.net/brunch/service/user/1YN0/image/ak-gRe29XA2HXzvSBowU7Tl7LFE.png"),
-    );
-  }
-}
-
-
-//PersonTile 부분
-class PersonTile extends StatelessWidget {
-  PersonTile(this._person);
-
-  final Person _person;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      trailing: Icon(Icons.arrow_left),
-      title: Text(_person.name),
-      subtitle: Text('${_person.age}살'),
-    );
-  }
-}
-
-class Person {
-  String name;
-  int age;
-
-  Person(this.name, this.age);
 }
